fc90d30c4daf909c9b37a7f630b9031f382a2249
matvey <matvetron@gmail.com>
2024-05-17 20:49:38 +0200

    style: Improve footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 import { HeartIcon } from '@heroicons/react/solid';
 
 const Footer = () => (
-  <footer className="container xl:max-w-screen-xl mx-auto p-6 mt-8 text-center">
+  <footer className="container xl:max-w-screen-xl mx-auto p-6 mt-8 text-center bg-gray-100">
     <p>
       <a
         href="https://strapex.org"
@@ -14,6 +14,8 @@ const Footer = () => (
         by Strapex Team
       </a>
     </p>
+    <p className="text-gray-500 mt-4">&copy; StarknetStore 2024</p>
+    <p className="text-gray-500 mt-2">Contact us at: <a href="mailto:contact@starknetstore.com" className="text-gray-500 hover:text-current">contact@starknetstore.com</a></p>
   </footer>
 );
 
