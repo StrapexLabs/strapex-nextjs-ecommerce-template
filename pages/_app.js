ed20be69a2b5289c77eca3929ac61f173dfc6f7b
mateo <mateosantana2024@gmail.com>
2024-07-25 18:17:16 +0200

    feat: add slink

--- pages/_app.js
+++ pages/_app.js
@@ -4,29 +4,27 @@ import { CartProvider } from '@/hooks/use-shopping-cart';
 import { Header, Footer } from '@/components/index';
 import { Toaster } from 'react-hot-toast';
 import CookieConsent from 'react-cookie-consent';
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>
-          Starknet Merch Store
-        </title>
-        <meta
-          name="description"
-          content="Starknet Store"
-        />
-        {/* */}
+        <title>Starknet Merch Store</title>
+        <meta name="description" content="Starknet Store" />
         <link rel="icon" href="/favicon.ico" />
         <meta property="og:title" content="Starknet Store" />
         <meta property="og:description" content="Starknet Store" />
         <meta property="og:image" content="https://starknetstore.com/StarknetStore.png" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://starknetstore.com" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Starknet Store" />
-        <meta name="twitter:description" content="Starknet Store" />
+        <meta name="twitter:card" content="player" />
+        <meta name="twitter:site" content="@StarknetStore" />
+        <meta name="twitter:title" content="Starknet Merch Store" />
+        <meta name="twitter:description" content="Official merchandise store for Starknet ecosystem" />
+        <meta name="twitter:player" content="https://starknetstore.com/twitter-card.html" />
+        <meta name="twitter:player:width" content="360" />
+        <meta name="twitter:player:height" content="560" />
         <meta name="twitter:image" content="https://starknetstore.com/StarknetStore.png" />
-
       </Head>
       <CartProvider>
         <body className="min-h-screen flex flex-col">
@@ -36,7 +34,7 @@ function MyApp({ Component, pageProps }) {
           </main>
           <Footer />
           <script async defer src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
-<noscript><img src="https://queue.simpleanalyticscdn.com/noscript.gif" alt="" referrerpolicy="no-referrer-when-downgrade" /></noscript>
+          <noscript><img src="https://queue.simpleanalyticscdn.com/noscript.gif" alt="" referrerpolicy="no-referrer-when-downgrade" /></noscript>
         </body>
       </CartProvider>
       <Toaster />
